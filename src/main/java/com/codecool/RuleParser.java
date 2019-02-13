757652d9df960e82fae0bb140db7004a922e0320
Károly Taraszovics <tarakarcsi94@gmail.com>
2019-02-13 09:35:25 +0100

    ruleparser v1

--- src/main/java/com/codecool/RuleParser.java
+++ src/main/java/com/codecool/RuleParser.java
@@ -3,4 +3,13 @@ package com.codecool;
 
 public class RuleParser extends XMLParser {
 
+
+    public void loadQuestions(){ //reading the questions part and making Question objects
+
+    }
+
+    public void loadAnswers() { //reading the answers part and making Answer objects
+
+    }
+
 }
